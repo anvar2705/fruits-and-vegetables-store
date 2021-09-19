5ddcde4c48b703f5a3fa5e56681e6b2c671a37df
AnvarAppazov <anvar978@yandex.ru>
2021-09-19 23:54:18 +0400

    Language drop-down list

--- scripts.js
+++ scripts.js
@@ -1,3 +1,6 @@
+
+
+//swiper slider
 const swiperLatest = new Swiper('.swiper-latest', {
     loop: true,
     speed: 800,
@@ -8,3 +11,27 @@ const swiperLatest = new Swiper('.swiper-latest', {
         prevEl: '.swiper-button-prev-latest',
     },
 });
+
+
+//custom select
+document.querySelector('.select').onclick = function (event) {
+    let selectHeader = event.target.closest('.select__header'),
+        selectItem = event.target.closest('.select__item'),
+        select = event.target.closest('.select'),
+        currentValue = document.querySelector('.section-header-language__text'),
+        currentValueIcon = document.querySelector('.section-header-language__icon')
+    if (selectHeader)
+        this.classList.toggle('select_is-active')
+
+    if (selectItem) {
+        currentValue.innerHTML = selectItem.children[1].innerHTML
+        currentValueIcon.src = selectItem.children[0].src
+        select.classList.remove('select_is-active')
+    }
+}
+document.onclick = function (event) {
+    let select = event.target.closest('.select')
+    if (!select) {
+        document.querySelector('.select').classList.remove('select_is-active')
+    }
+}
